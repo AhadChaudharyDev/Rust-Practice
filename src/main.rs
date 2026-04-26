452b1546b79c951847a36039c2627fd8bcf788f6
AhadChaudharyDev <ahadchaudhery@gmail.com>
2026-04-26 13:18:07 +0500

    Refactor main function to include function calls for hello_world and variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod exercises;
+
+
 fn main(){
     
+    // Function call
+    exercises::common_programming_concepts::hello_world::hello();
+    exercises::common_programming_concepts::variables_and_mutability::variables();
+    
+
 }
