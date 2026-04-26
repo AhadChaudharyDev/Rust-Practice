4d04d29895e0e07bc2708b542c7b2f0ec9f303be
AhadChaudharyDev <ahadchaudhery@gmail.com>
2026-04-26 15:31:45 +0500

    cover integer type inference

---
+++ src/exercises/Common_Programming_Concepts/data_types.rs
@@ -0,0 +1,19 @@
+pub fn start() {
+    // ================================================
+    // Scalor types
+    // =================================================
+
+    println!("===============================");
+
+    // =================================================
+    // Integer type inference
+    // =================================================
+
+    println!("Integer type inference");
+
+    // If we don't specify a type, Rust infers it.
+    // By default, integer literals are inferred as i32.
+
+    let x = 10;
+    println!("x = {x}");
+}
