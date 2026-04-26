81f34d4dd236bc8d6a49e21e00596599d669447e
AhadChaudharyDev <ahadchaudhery@gmail.com>
2026-04-26 13:26:48 +0500

    rustfmt code

--- src/exercises/Common_Programming_Concepts/hello_world.rs
+++ src/exercises/Common_Programming_Concepts/hello_world.rs
@@ -1,4 +1,4 @@
-pub fn hello(){
+pub fn hello() {
     println!("Hello, World");
     println!("This is Ahad Chaudhary");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 mod exercises;
 
-
-fn main(){
-    
+fn main() {
     // Function call
     exercises::common_programming_concepts::hello_world::hello();
     exercises::common_programming_concepts::variables_and_mutability::variables();
-    
-
 }
