773221a18acd344ed8da043e57bd0431245c523a
AhadChaudharyDev <ahadchaudhery@gmail.com>
2026-04-26 13:26:01 +0500

    add constant variable

--- src/exercises/Common_Programming_Concepts/variables_and_mutability.rs
+++ src/exercises/Common_Programming_Concepts/variables_and_mutability.rs
@@ -1,34 +1,33 @@
-pub fn variables(){
-    
-    
+pub fn variables() {
     // By default variable in rust are immutable
     // we cannot modify immutable variable
     // If we want to make it mut then we use (mut) keyword
-
 
     // Mutable variable
     println!("==========Mutable Variable==========");
     let mut _x = 10;
     _x = 100;
-    println!("The Value of x is {}",_x);
-    
-
-
+    println!("The Value of x is {}", _x);
 
     // Immutable variable
     println!("==========Mutable Variable==========");
     let y = 10;
-    println!("The value of y is {}",y);
-
+    println!("The value of y is {}", y);
 
     // Shadowing
 
+    println!("==========Shadowing==========");
     let z = 10;
     {
-        let z='c';
-        println!("the value of z in inner scope is {}",z);        
-        
+        let z = 'c';
+        println!("the value of z in inner scope is {}", z);
     }
-        println!("the value of z in outer scope is {}",z);        
+    println!("the value of z in outer scope is {}", z);
 
+    // Constant
+
+    println!("==========Constant==========");
+    const VALUE_OF_PI: f64 = 3.1415;
+
+    println!("The Value of pi is {}", VALUE_OF_PI);
 }
