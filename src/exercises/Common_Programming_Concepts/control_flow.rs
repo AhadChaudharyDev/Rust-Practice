e0a93c6a863c53592bfddc1bad0d7df28eb121d3
AhadChaudharyDev <ahadchaudhery@gmail.com>
2026-04-27 16:31:21 +0500

    add control_flow function to demonstrate if statement usage

---
+++ src/exercises/Common_Programming_Concepts/control_flow.rs
@@ -0,0 +1,14 @@
+pub fn control_flow() {
+    // =============================
+    // if statement
+    // =============================
+
+    println!("if condition");
+
+    let x = 10;
+    if x < 5 {
+        println!("The value of x is less than 5");
+    } else {
+        println!("The value of x is greater than 5");
+    }
+}
