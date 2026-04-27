0506aee84f82b8611737bbcf5a1c018c2db2a1f9
AhadChaudharyDev <ahadchaudhery@gmail.com>
2026-04-27 16:33:28 +0500

    add control_flow function call in main

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     exercises::common_programming_concepts::variables_and_mutability::variables();
     exercises::common_programming_concepts::data_types::start();
     exercises::common_programming_concepts::functions::fun();
+    exercises::common_programming_concepts::control_flow::control_flow();    
 }
