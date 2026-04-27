9d81eb33058dd3369ebde256ed76d97951079399
AhadChaudharyDev <ahadchaudhery@gmail.com>
2026-04-27 15:24:59 +0500

    statement and expression

--- src/exercises/Common_Programming_Concepts/functions.rs
+++ src/exercises/Common_Programming_Concepts/functions.rs
@@ -1,9 +1,33 @@
 pub fn fun() {
-    println!("Hello");
+    // ==================================={}
+    // Statements and Expressions
+    // ===================================
 
-    another_function();
+    // Rust is an expression-based language
+
+    // let x = y = z = 10;
+    // Not allowed because assignment is a statement in Rust
+    // unlike C/C++ and Ruby
+
+    let y = {
+        let x = 10;
+        x + 1 // last expression (no semicolon) returns the value
+    };
+
+    println!("y = {y}");
+
+    println!("-----------------");
+
+    // ===================================
+    // Parameters
+    // ===================================
+
+    println!("Hello");
+    another_function("developers");
+
+    println!("----------------");
 }
 
-fn another_function() {
-    println!("This is Ahad chaudhary");
+fn another_function(x: &str) {
+    println!("Rustaceans {}!!!", x);
 }
