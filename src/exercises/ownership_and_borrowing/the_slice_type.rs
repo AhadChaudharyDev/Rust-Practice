99979cd3f531a264dab3e3aee06ad5c74a5dad7b
AhadChaudharyDev <ahadchaudhery@gmail.com>
2026-04-28 12:24:48 +0500

    looking for second word

--- src/exercises/ownership_and_borrowing/the_slice_type.rs
+++ src/exercises/ownership_and_borrowing/the_slice_type.rs
@@ -7,6 +7,10 @@ pub fn slice_type() {
     // Find first word using string slice
     let f_word = first_word(&s);
     println!("first word = {f_word}");
+
+    // Find second word
+    let s_word = second_word(&s);
+    println!("second word = {s_word}");
 }
 
 // ---------------------------------
@@ -30,4 +34,36 @@ fn first_word(s: &str) -> &str {
 
     // If no space found, whole string is one word
     &s[..]
+}
+
+// ---------------------------------
+// Returns the second word of a string
+// ---------------------------------
+fn second_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    let mut first_space = None;
+
+    // Step 1: Find index of first space
+    for (index, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            first_space = Some(index);
+            break;
+        }
+    }
+    // If no space exists, there is no second word
+    let start = match first_space {
+        Some(i) => i + 1, // start after first space
+        None => return "",
+    };
+
+    // Step 2: Find next space after first word
+    for (index, &item) in bytes[start..].iter().enumerate() {
+        if item == b' ' {
+            // Slice from second word start to next space
+            return &s[start..start + index];
+        }
+    }
+
+    // If no second space found, return till end
+    &s[start..]
 }
